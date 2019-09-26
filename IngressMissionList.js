676239c0cdc343ed7e7d51d0e9b8c89b884e8587
aysav <aysav@tut.by>
2019-09-26 11:03:15 +0300

    0.2 vers

--- IngressMissionList.js
+++ IngressMissionList.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         IngressMissionList
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       aysav
-// @match        https://mission-author-dot-betaspike.appspot.com/
+// @match        https://mission-author-dot-betaspike.appspot.com
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
@@ -16,42 +16,47 @@
 (function() {
     'use strict';
     var $ = window.jQuery;
-    var maindiv = $(".navbar-header");
-    var btnlist = [
-                     {caption: "All", func: "btn-mis-all"},
-                     {caption: "Published", func: "btn-mis-pub"},
-                     {caption: "Submitted", func: "btn-mis-sub"},
-                     {caption: "Draft", func: "btn-mis-draft"}
-                  ];
+    var viewBtn = true;
 
-    btnlist.forEach(function(el) {
-        var b = $("<button class='yellow  btn-show' style='margin-top:5px;' func='" + el.func +"'>" + el.caption + "</button>");
-        //maindiv.prepend(b);
-        maindiv.after(b);
-    });
+    if (viewBtn){
+      var maindiv = $(".navbar-header");
+      var btnlist = [
+                     {
+                         caption: "All",
+                         func_id: "btn-mis-all",
+                         func: function (){$(".missions-list>div").show();}
+                     },
+                     {
+                         caption: "Published",
+                         func_id: "btn-mis-pub",
+                         func: function (){$(".missions-list>div").hide();$(".mission-list-item-published").show();}
+                     },
+                     {
+                         caption: "Submitted",
+                         func_id: "btn-mis-sub",
+                         func: function (){$(".missions-list>div").hide();$(".mission-list-item-submitted").show();}
+                     },
+                     {
+                         caption: "Draft",
+                         func_id: "btn-mis-draft",
+                         func: function (){$(".missions-list>div").hide();$(".mission-list-item-draft").show();}
+                     }
+                   ];
 
-    $('.btn-show').click(function (){
+      btnlist.forEach(function(el) {
+          var b = $("<button class='yellow  btn-show' style='margin-top:5px;' func_id='" + el.func_id +"'>" + el.caption + "</button>");
+          maindiv.append(b);
+      });
+
+      $('.btn-show').click(function (){
         $('.btn-show').removeClass("green").addClass("yellow");
         $(this).addClass("green").removeClass("yellow");
+        var f = $(this).attr("func_id");
 
-        var f = $(this).attr("func");
-
-        switch(f){
-            case "btn-mis-all":
-                $(".missions-list>div").show();
-                break;
-            case "btn-mis-pub":
-                $(".missions-list>div").hide();
-                $(".mission-list-item-published").show();
-                break;
-            case "btn-mis-sub":
-                $(".missions-list>div").hide();
-                $(".mission-list-item-submitted").show();
-                break;
-            case "btn-mis-draft":
-                $(".missions-list>div").hide();
-                $(".mission-list-item-draft").show();
-                break;
+        var obj = btnlist.filter(el => el.func_id == f);
+        if (obj.length){
+          obj[0].func();
         }
-    });
+      });
+   }
 })();
